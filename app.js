18b459d60487128e6068eeb7e0d1574f5232d8e8
madhankumar2211 <madhankumar199722@gmail.com>
2021-10-15 16:08:42 +0530

    Initaial commit

--- app.js
+++ app.js
@@ -5,20 +5,28 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+//session
+var session = require('express-session');
+app.use(session({
+                    secret: "Shh, its a secret!",
+                    resave: false,
+                    saveUninitialized: true
+                }))
+
 const {MongoClient,ObjectId} = require('mongodb');
 const url = 'mongodb://127.0.0.1:27017/';
 
-//Home page
-app.get('/',(req,res) => {
-    res.send('Hi welcome')
-});
+const cors = require('cors');
+app.use(cors({
+    origin:["http://localhost:4200"],credentials:true
+}));
 
-//View
+
+//View 
 app.get('/allusers',(req,res) => {
     MongoClient.connect(url,(err,conn) => {
         var db = conn.db('merit');
-        db.collection('packers').find().toArray((err,data) => {
-            console.log(data);
+        db.collection('packers_users').find().toArray((err,data) => {
             res.send(data)
         })
     })
@@ -26,59 +34,141 @@ app.get('/allusers',(req,res) => {
 
 
 //create
-app.post('/adduser',(req,res) => {
+app.post('/register',(req,res) => {
     console.log(req.body);
     MongoClient.connect(url,(err,conn) => {
         var db = conn.db('merit');
-        db.collection('packers').insertOne(req.body,(err,data) =>{
-            if(err){
-                console.log(err);
-                res.send(err)
-            }else{
-                res.send(data);
+        db.collection('packers_users').find({email: req.body.email}).toArray((err,data) =>{
+            if(err)
+            {
+                res.send(err);
+            }
+            else{
+                if(data.length == 1){
+                    res.send('Email already registered with us.');
+                }
+                else
+                {
+                    db.collection('packers_users').insertOne(req.body,(err,data) =>{
+                        if(err){
+                            console.log(err);
+                            res.send(err)
+                        }else{
+                            res.send(data);
+                        }
+                    });
+                }
             }
         });
     });
 })
 
-//read
+//login
 
-app.get('/user/:id',(req,res) => {
+app.post('/login',(req,res) => {
+    console.log(req.body);
     MongoClient.connect(url,(err,conn) => {
-        var db = conn.db('merit');
-        db.collection('packers').find({_id : ObjectId(req.params.id)}).toArray((err,data) => {
-            res.send(data)
-        })
-    })
-})
-
-
-//update
-
-app.patch('/updateuser/:id',(req,res) => {
-    MongoClient.connect(url,(err,conn) => {
-        var db = conn.db('merit');
-        db.collection('packers').updateOne(
-            {_id : ObjectId(req.params.id)},
-            {$set:req.body},
-            (err,data) => {
-                res.send(data)
+            var db = conn.db('merit');
+            db.collection('packers_users').find({email: req.body.email}).toArray((err,data) =>{
+            if(data.length === 0){
+                res.send("Incorrect email or Password");
             }
-        )
-    })
+            else {
+                if(data[0].psw === req.body.psw){
+                        console.log(data[0]);
+                        req.session.email = req.body.email;
+                        req.session.psw = req.body.psw;
+                        req.session.uid = data[0]._id;
+                        res.status(200).send(data[0]);   
+                }else{
+                    res.send('Incorrect email or Password');
+                }
+            }
+        });
+    });
+});
+
+
+app.post('/logout',(req,res) =>{
+    console.log(req.session.email);
+    console.log(req.session.psw);
+    console.log(req.session.uid);
+    if(req.session.email){
+        req.session.destroy((err) =>{
+            if(err){
+                res.status(500).send('Could not log out.' )
+            }else{
+                res.status(200).send({})
+            }
+        });
+    }else{
+        res.status(404).send({})
+    }
 })
 
-//delete
-app.delete('/deleteuser/:id',(req,res) => {
+//forgot
+app.post('/forgot',(req,res) => {
     MongoClient.connect(url,(err,conn) => {
         var db = conn.db('merit');
-        db.collection('packers').findOneAndDelete({_id : ObjectId(req.params.id)},(err,data) => {
-            res.send(data)
+        db.collection('packers_users').find({email : req.body.email}).toArray((err,data) => {
+            if(err){
+                res.send(err)
+            }
+            else{
+                if(data.length === 0)
+                {
+                    res.send('User not found')
+                }
+                else{
+                    res.status(200).send(data[0]._id)
+                }
+            }
         })
     })
 })
 
+//update password
+app.put('/updatepassword',(req,res) => {
+    console.log(req.body);
+    MongoClient.connect(url,(err,conn) => {
+        var db = conn.db('merit');
+        db.collection('packers_users')
+        .updateOne(
+            {email : req.body.email},
+            {$set: {psw : req.body.psw}},
+            (err,data) =>{
+                if(err){
+                    res.send(err);
+                }
+                else{
+                    res.send(data);
+                }
+        });
+    });
+});
 
+function ath(req,res,next){
+    console.log(req.session.uid);
+    if(req.session.email){
+        MongoClient.connect(url,(err,conn) => {
+            var db = conn.db('merit');
+            db.collection('packers_users').find({email : req.session.email}).toArray((err,data) =>{
+                if(data[0].psw === req.session.psw){
+                    next()
+                }
+                else{
+                    res.send({link : '/login'})
+                }
+            });
+        });
+    }else{
+        res.send({link : '/login'})
+    }
+}
+
+app.get('/test',ath,(req,res) => {
+    res.send({link : '/test'})
+})
 
 
 app.listen(7080,() => {
